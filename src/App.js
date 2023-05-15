f6d2d993fb7a00e92444c38fc86fea7af3d4b7db
Alireza <arbor0711@gmail.com>
2023-05-15 23:56:31 +0200

    Excellent example of useState and props to manage the data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
 import React, { useState } from "react";
 
-function App() {
-  const [quote, setQuote] = useState(null);
+function GoalForm(props) {
+  const [formData, setFormData] = useState({ goal: "", by: "" });
 
-  const eventHandler = (e) => setQuote(e.target.value);
+  function changeHandler(e) {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  }
+
+  function submitHandler(e) {
+    e.preventDefault();
+    props.onAdd(formData); //SHOULD LEARN
+    setFormData({ goal: "", by: "" });
+  }
 
   return (
-    <section>
-      <div>
-        <lable>Enter your question here.</lable>
-        <br></br>
-        <textarea onChange={eventHandler} type="text" />
-      </div>
-      {quote ? (
-        <p>
-          <b>You have asked: </b>
-          <em>{quote}</em>
-        </p>
-      ) : null}
-    </section>
+    <>
+      <h1>My Little Lemon Goals</h1>
+      <form onSubmit={submitHandler}>
+        <input
+          type="text"
+          placeholder="Goal"
+          name="goal"
+          onChange={changeHandler}
+          value={formData.goal}
+        />
+        <input
+          type="text"
+          placeholder="by..."
+          name="by"
+          onChange={changeHandler}
+          value={formData.by}
+        />
+        <button>Submit Goal</button>
+      </form>
+    </>
   );
 }
 
-export default App;
+function ListOfGoals(props) {
+  return (
+    <ul>
+      {props.allGoals.map((g) => (
+        <li key={g.goal}>
+          <span>
+            My goal is to {g.goal}, by {g.by}
+          </span>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default function APP() {
+  const [allGoals, setAllGoals] = useState([]);
+
+  function addGoal(goal) {
+    setAllGoals([...allGoals, goal]);
+  }
+
+  return (
+    <div className="App">
+      <GoalForm onAdd={addGoal} />
+      <ListOfGoals allGoals={allGoals} />
+    </div>
+  );
+}
